e53c0a6885adfbad2b130f561e83fd390921ec4b
Dung <dung0158234@gmail.com>
2023-12-04 23:52:31 +0700

    Upload file

---
+++ src/main/java/model/bean/fileUpload.java
@@ -0,0 +1,10 @@
+package model.bean;
+
+public class fileUpload {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/model/bo/UploadfileBO.java
@@ -0,0 +1,10 @@
+package model.bo;
+
+public class UploadfileBO {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
